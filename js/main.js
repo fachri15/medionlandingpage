6a95f112dae17e8203cbb26b28d9cd1ab57f97ab
fachri15 <fachridatulotolomo1508@gmail.com>
2023-12-22 21:45:21 +0800

    medion landing page

---
+++ js/main.js
@@ -0,0 +1,110 @@
+$(function () {
+
+	 	var $nav = $(".navbar-fixed-top");
+		var $logo = $(".navlogo");
+
+		$(document).scroll(function () {
+			var scrolled = $(this).scrollTop() > $nav.height();
+
+			$nav.toggleClass("scrolled", scrolled);
+
+			if (scrolled) {
+			$nav.css("color", "#000");
+			} else {
+			$nav.css("color", "#fff");
+			}
+
+			// Ganti logo
+			if (scrolled) {
+			$logo.attr("src", "img/logorastw.png");
+			} else {
+			$logo.attr("src", "img/logorastw.png");
+			}
+		});
+
+  $('.testimoni-wrap').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: true,
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		vertical:true,
+		verticalSwiping:true,
+		autoplaySpeed: 6000,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow:1,
+		        slidesToScroll: 1,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },{
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  
+  		]
+	});
+
+	$('.testimoni-wrap-2').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		infinite: true,
+		dots: true,
+		arrows:false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow:2,
+		        slidesToScroll:2,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },{
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  
+  		]
+	});
+});
